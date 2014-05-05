e3ac2e89868e0cbf1aa93736e60274d3cc79c573
foxt <tyler.fox09@gmail.com>
2014-05-05 03:05:56 -0400

    finished backend coordinates

--- CelebSearch/CelebResults/views.py
+++ CelebSearch/CelebResults/views.py
@@ -29,16 +29,25 @@ def results(request):
 
         ## Search tweets in mongo
         tweets = mongodb.search_tweets(name)
-        
+
         ## If none found, search twitter
         if len(tweets) == 0:
             tweets = clips.search_tweets(name)
 
+        coords = []
         for tweet in tweets:
             tweet["text"]=tweet["text"].encode('ascii','ignore')
             tweet["text"]=tweet["text"].replace('"','')
+            try:
+                curr_coords = tweet["coordinates"]
+                if curr_coords["type"] == 'Point':
+                    coords.append(curr_coords["coordinates"])
+            except:
+                coords.append([-200,-200])
+
+        print coords
         ## Score the tweets with sentiment analysis
         scores = clips.get_sentiment(tweets)
-        context = RequestContext(request, {'tweets':tweets, 'scores': scores})
+        context = RequestContext(request, {'tweets':tweets, 'scores': scores, 'coordinates': coords})
         return render_to_response('CelebResults/results.html', context)
 
